3b07ab0c822125c6d965df4a1d791d54a2e77ee1
Ryan Bartelme <r.p.bartelme@gmail.com>
2020-12-03 10:43:13 -0700

    day03 solution attempt 1

---
+++ Day03.R
@@ -0,0 +1,27 @@
+library(stringr)
+
+#read in as character vector with each line being an element of the vector
+day3_in <- readLines(con = "~/AdventOfCode2020/inputs/day3.txt", warn = FALSE)
+
+#string split into matrix, convert to dataframe for logical indexing
+day3_mat <- as.data.frame(str_split(string = day3_in,
+                                    pattern = "", simplify = TRUE))
+#make vector to catch output
+traveled <- vector(mode = "character", length = 10)
+
+#add initial starting position values
+#column
+y <- 1
+#row
+x <- 1
+#for loop to walk through text & catch output in character vector
+for(n in 1:length(traveled)){
+  traveled[n] <- day3_mat[x, y]
+  #walk to the right 3
+  y <- y + 3
+  #walk down 1 row
+  x <- x + 1
+}
+#loop ended on last column, so this next line will count trees
+length(which(traveled == "#"))
+# [1] 5
