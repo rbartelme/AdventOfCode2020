8f721bda1d1e41913dec6e63f8a59ea782fd73d1
Ryan Bartelme <r.p.bartelme@gmail.com>
2020-12-03 16:28:29 -0700

    brute force solution

--- Day03.R
+++ Day03.R
@@ -7,16 +7,22 @@ day3_in <- readLines(con = "~/AdventOfCode2020/inputs/day3.txt", warn = FALSE)
 day3_mat <- as.data.frame(str_split(string = day3_in,
                                     pattern = "", simplify = TRUE))
 #make vector to catch output
-traveled <- vector(mode = "character", length = 10)
-
+traveled <- vector(mode = "character", length = nrow(day3_mat))
+#collumn bind to walk across the width
+day3_cat <- cbind(day3_mat, day3_mat, day3_mat, day3_mat, day3_mat,
+      day3_mat, day3_mat, day3_mat, day3_mat, day3_mat, day3_mat,
+      day3_mat, day3_mat, day3_mat, day3_mat, day3_mat, day3_mat,
+      day3_mat, day3_mat, day3_mat, day3_mat, day3_mat, day3_mat,
+      day3_mat, day3_mat, day3_mat, day3_mat, day3_mat, day3_mat,
+      day3_mat, day3_mat, day3_mat, day3_mat, day3_mat, day3_mat)
 #add initial starting position values
 #column
 y <- 1
 #row
 x <- 1
 #for loop to walk through text & catch output in character vector
-for(n in 1:length(traveled)){
-  traveled[n] <- day3_mat[x, y]
+for(n in 1:nrow(day3_cat)){
+  traveled[n] <- day3_cat[x, y]
   #walk to the right 3
   y <- y + 3
   #walk down 1 row
